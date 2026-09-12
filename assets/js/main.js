4f27fdde1ba4dd612e3db09fba39d5d498bf8cbd
nikpendley <nikolaspendley@gmail.com>
2026-09-12 01:10:10 -0500

    hover update

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a.image',
+					selector: '.work-item a.lightbox',
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
